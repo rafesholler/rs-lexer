90914e0bef1281a8b232222797b72f8bcd8936f5
Rafe Sholler <rafe.sholler@gmail.com>
2025-10-20 14:26:57 -0400

    Add a few tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 mod token;
 use token::Token;
 
-pub fn tokenize(data: String, file: String) -> Result<Vec<Token>, Box<dyn Error>> {
+pub fn tokenize(data: &str, file: &str) -> Result<Vec<Token>, Box<dyn Error>> {
     let mut token_patterns = std::collections::HashMap::new();
     
     let mut split_data = data.split_whitespace();
@@ -12,7 +12,6 @@ pub fn tokenize(data: String, file: String) -> Result<Vec<Token>, Box<dyn Error>
     while let Some(regex) = split_data.next() {
         if let Some(token_name) = split_data.next() {
             let mut new_regex = String::from(regex);
-            new_regex.push_str("$");
             new_regex.insert_str(0, "^");
             token_patterns.insert(String::from(new_regex), token_name.to_uppercase());
         } 
@@ -20,27 +19,26 @@ pub fn tokenize(data: String, file: String) -> Result<Vec<Token>, Box<dyn Error>
 
     let mut tokens = vec![];
 
-    let mut i = 0;
+    let mut buffer = &file[0..file.len()];
 
-    while i < file.len() {
+    while buffer.len() > 0 {
         let mut found = false;
         for t_pattern in &token_patterns {
-            if (i + t_pattern.0.len() - 2) >= file.len() {
-                continue;
-            }
-
-            let word = &file[i..(i + t_pattern.0.len() - 2)];            
+            
+            
             let re = Regex::new(&t_pattern.0)?;
-            if re.is_match(word) {
-                tokens.push(Token::new(t_pattern.1.to_string(), word.to_owned()));
+            if let Some(word) = re.find(buffer) {
+                tokens.push(Token::new(t_pattern.1.to_string(), word.as_str().to_string()));
                 found = true;
-                i += word.len();
+                buffer = &buffer[(word.as_str().len())..buffer.len()];
                 break;
             }
         }
         if !found {
-            i += 1;
-            tokens.push(Token::new("RAW".to_string(), file[i..i+1].to_string()))
+            if !buffer[0..1].contains(char::is_whitespace) {
+                tokens.push(Token::new("raw".to_string(), buffer[0..1].to_string()));
+            }
+            buffer = &buffer[1..buffer.len()];
         }
     }
 
@@ -53,6 +51,56 @@ mod tests {
 
     #[test]
     fn test_empty() {
-        assert_eq!(tokenize(String::new(), String::new()).unwrap(), vec![]);
+        assert_eq!(tokenize("", "").unwrap(), vec![]);
+    }
+
+    #[test]
+    fn test1() {
+        let lex = std::fs::read_to_string("src/test/test_lex1.txt").unwrap();
+        let file = std::fs::read_to_string("src/test/test_file1.txt").unwrap();
+
+        let output = tokenize(&lex, &file).unwrap();
+
+        let expected = vec![
+            Token::new("LITERAL".to_string(), "This".to_string()),
+            Token::new("LITERAL".to_string(), "tests".to_string()),
+            Token::new("LITERAL".to_string(), "words".to_string()),
+            Token::new("LITERAL".to_string(), "and".to_string()),
+            Token::new("LITERAL".to_string(), "integers".to_string()),
+            Token::new("LITERAL".to_string(), "like".to_string()),
+            Token::new("NUM".to_string(), "9000".to_string()),
+            Token::new("NUM".to_string(), "-7474".to_string()),
+            Token::new("LITERAL".to_string(), "and".to_string()),
+            Token::new("NUM".to_string(), "22222".to_string()),
+        ];
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test2() {
+        let lex = std::fs::read_to_string("src/test/test_lex2.txt").unwrap();
+        let file = std::fs::read_to_string("src/test/test_file2.txt").unwrap();
+
+        let output = tokenize(&lex, &file).unwrap();
+
+        let expected = vec![
+            Token::new("LIT".to_string(), "Testing".to_string()),
+            Token::new("LINE_BREAK".to_string(), "\n".to_string()),
+            Token::new("LIT".to_string(), "more".to_string()),
+            Token::new("LEFT_PAREN".to_string(), "(".to_string()),
+            Token::new("LIT".to_string(), "like".to_string()),
+            Token::new("LIT".to_string(), "parenthases".to_string()),
+            Token::new("RIGHT_PAREN".to_string(), ")".to_string()),
+            Token::new("LIT".to_string(), "and".to_string()),
+            Token::new("LINE_BREAK".to_string(), "\n".to_string()),
+            Token::new("LINE_BREAK".to_string(), "\n".to_string()),
+            Token::new("LIT".to_string(), "whitespace".to_string()),
+            Token::new("LIT".to_string(), "patterns".to_string()),
+            Token::new("LIT".to_string(), "like".to_string()),
+            Token::new("LIT".to_string(), "these".to_string()),
+        ];
+
+        assert_eq!(output, expected);
     }
 }
